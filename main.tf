f876d96c5614056114ef74aafd64f47223e44f46
domingobishop <domingobishop@gmail.com>
2021-01-06 10:41:20 +0000

    fix lambda function source path

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 data "archive_file" "lambda_zip" {
-    type = "zip"
-    source_dir = "lambda"
+    type        = "zip"
+    source_dir  = "${path.module}/lambda"
     output_path = "lambda-function.zip"
 }
 
@@ -25,11 +25,11 @@ EOF
 
     tags = {
         Environment = var.environment
-        Service = var.service
-        CostCentre = var.cost_centre
-        Owner = var.owner
-        CreatedBy = var.created_by
-        Terraform = true
+        Service     = var.service
+        CostCentre  = var.cost_centre
+        Owner       = var.owner
+        CreatedBy   = var.created_by
+        Terraform   = true
     }
 }
 
@@ -100,19 +100,19 @@ EOF
 }
 
 resource "aws_lambda_function" "out_of_hours_shutdown" {
-    filename = "lambda-function.zip"
-    function_name = "out-of-hours-shutdown"
-    role = aws_iam_role.out_of_hours_shutdown_lambda_role.arn
-    handler = "out_of_hours_shutdown.handler"
+    filename         = "lambda-function.zip"
+    function_name    = "out-of-hours-shutdown"
+    role             = aws_iam_role.out_of_hours_shutdown_lambda_role.arn
+    handler          = "out_of_hours_shutdown.handler"
     source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-    runtime = "python3.6"
+    runtime          = "python3.6"
 
     tags = {
         Environment = var.environment
-        Service = var.service
-        CostCentre = var.cost_centre
-        Owner = var.owner
-        CreatedBy = var.created_by
-        Terraform = true
+        Service     = var.service
+        CostCentre  = var.cost_centre
+        Owner       = var.owner
+        CreatedBy   = var.created_by
+        Terraform   = true
     }
 }
